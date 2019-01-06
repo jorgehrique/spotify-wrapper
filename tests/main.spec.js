7c38e14d89b5ee1760d38b1fa4694f40b9a8291c
Jorge Henrique <jorgehasd@gmail.com>
2019-01-06 16:51:13 -0200

    adding searchs tests and implements

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -9,6 +9,16 @@ sinonStubPromise(sinon)
 global.fetch = require('node-fetch')
 
 describe('Spotify Wrapper', () => {
+  let fetchedStub, promisse
+
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch')
+    fetchedStub.resolves({ body: () => { } });
+  })
+
+  afterEach(() => {
+    fetchedStub.restore()
+  })
 
   describe('Smoke Tests', () => {
     it('should exist a method ´search´', () => {
@@ -33,17 +43,6 @@ describe('Spotify Wrapper', () => {
   })
 
   describe('Generic Search', () => {
-    let fetchedStub, promisse
-
-    beforeEach(() => {
-      fetchedStub = sinon.stub(global, 'fetch')
-      fetchedStub.resolves({ body: () => { } });
-    })
-
-    afterEach(() => {
-      fetchedStub.restore()
-    })
-
     it('should call fetch function', () => {
       const artists = search();
       expect(fetchedStub).to.have.been.calledOnce;
@@ -66,14 +65,74 @@ describe('Spotify Wrapper', () => {
         expect(fetchedStub).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album')
       })
-
-      it('should return the JSON data from the promisse', () => {
-        const artists = search('Incubus', 'artist')
-        expect(artists.resolveValue).to.be.eql({ json: () => { } })
-      });
-
     });
 
+    it.skip('should return the JSON data from the promisse', () => {
+      const artists = search('Incubus', 'artist')
+      expect(artists.resolveValue).to.be.eql({ json: () => { } })
+    });
 
   })
+
+  describe('Search Artist', () => {
+    it('should call fetch function', () => {
+      const artists = searchArtists('Incubus')
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct url', () => {
+      const artists = searchArtists('Incubus')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist')
+
+      const artists2 = searchArtists('Muse')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Muse&type=artist')       
+    });
+  });
+
+  describe('Search Album', () => {
+    it('should call fetch function', () => {
+      const album = searchAlbums('Make Yourself')
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct url', () => {
+      const album = searchAlbums('Make Yourself')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Make Yourself&type=album')
+
+      const album2 = searchAlbums('Morning View')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Morning View&type=album')       
+    });
+  });
+
+  describe('Search Track', () => {
+    it('should call fetch function', () => {
+      const track = searchTracks('Southern Girl')
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct url', () => {
+      const track = searchTracks('Southern Girl')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Southern Girl&type=track')
+
+      const track2 = searchTracks('Are you in?')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Are you in?&type=track')       
+    });
+  });
+
+  describe('Search Playlist', () => {
+    it('should call fetch function', () => {
+      const playlist = searchPlaylists('This is Incubus')
+      expect(fetchedStub).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct url', () => {
+      const playlist = searchPlaylists('This is Incubus')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=This is Incubus&type=playlist')
+
+      const playlist2 = searchPlaylists('Best of Incubus')
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Best of Incubus&type=playlist')       
+    });
+  });
+
+
 }) 
